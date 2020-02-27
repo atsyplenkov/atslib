f81006a5f16fcba97f7ea0d052d1eae6af7cb4a7
atsyplenkov <atsyplenkov@gmail.com>
2020-02-27 12:18:01 +0300

    additional functions

--- R/ggthemes.R
+++ R/ggthemes.R
@@ -75,3 +75,51 @@ Add_R2 <- function(method = "lm",
                         ...)
 
 }
+
+#' A ggplot theme for maps
+#'
+#' @param ... Other arguments passed to \code{theme_minimal}
+#'
+#' @details This is a slightly modified Timo Grossenbacher theme from
+#' this tutorial \url{https://timogrossenbacher.ch/2018/03/categorical-spatial-interpolation-with-r/}
+#'
+#' @export
+
+theme_map <- function(...) {
+  ggplot2::theme_minimal() +
+    ggplot2::theme(
+      text = ggplot2::element_text(family = "Ubuntu", color = "#22211d"),
+      # remove all axes
+      axis.line = ggplot2::element_blank(),
+      # axis.text.x = element_blank(),
+      # axis.text.y = element_blank(),
+      axis.ticks = ggplot2::element_blank(),
+      axis.title.x = ggplot2::element_blank(),
+      axis.title.y = ggplot2::element_blank(),
+      # add a subtle grid
+      panel.grid.major = ggplot2::element_line(color = "#dbdbd6", size = 0.6),
+      panel.grid.minor = ggplot2::element_blank(),
+      plot.background = ggplot2::element_rect(fill = "#ffffff", color = NA),
+      plot.margin = ggplot2::unit(c(.5, .5, .2, .5), "cm"),
+      panel.border = ggplot2::element_blank(),
+      panel.background = ggplot2::element_rect(fill = "#ffffff", color = NA),
+      panel.spacing = ggplot2::unit(c(-.1, 0.2, .2, 0.2), "cm"),
+      legend.background = ggplot2::element_rect(fill = "#ffffff", color = NA),
+      legend.title = ggplot2::element_text(size = 13),
+      legend.text = ggplot2::element_text(size = 11, hjust = 0, color = "#4e4d47"),
+      plot.title = ggplot2::element_text(size = 16, hjust = 0.5, color = "#4e4d47"),
+      plot.subtitle = ggplot2::element_text(size = 12, hjust = 0.5, color = "#4e4d47",
+                                            margin = ggplot2::margin(b = -0.1,
+                                                                     t = -0.1,
+                                                                     l = 2,
+                                                                     unit = "cm"),
+                                            debug = F),
+      plot.caption = ggplot2::element_text(size = 9,
+                                           hjust = .5,
+                                           margin = ggplot2::margin(t = 0.2,
+                                                                    b = 0,
+                                                                    unit = "cm"),
+                                           color = "#939184"),
+      ...
+    )
+}
