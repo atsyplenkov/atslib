21f9a7e6800f83d3e0606164e159f981f61a5675
Anatolii Tsyplenkov <34775595+atsyplenkov@users.noreply.github.com>
2021-03-02 10:23:43 +0300

    smart_round function

---
+++ R/smart_round.R
@@ -0,0 +1,16 @@
+#' @title Rounding to 3 signif digits
+#'
+#' @description This function rounds decimal values to a 3 signif digits.
+#' @param x numerical value
+#' @keywords round
+#' @export
+#' @examples
+#' smart_round(3.4699878)
+
+smart_round <- function(x){
+  dplyr::case_when(
+    x < 10 ~ signif(x, 3),
+    dplyr::between(x, 10, 100) ~ round(x, 1),
+    x >=100 ~ round(x)
+  )
+}
